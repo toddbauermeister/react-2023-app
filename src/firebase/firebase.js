d6005e5a1a1dc06a5ccff5b86fddc351fea52c1a
Todd Bauermeister <todd@octoco.ltd>
2023-07-16 18:23:41 +0200

    add base firebase auth functions

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,6 +3,8 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/analytics'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { logEvent } from "firebase/analytics";
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -30,3 +32,37 @@ export const analytics = firebase.analytics(firebaseApp);
 
 // Create a Database Variable (Firestore) and get a reference to the service
 export const database = firebaseApp.firestore();
+
+export const firebaseSignup = async (email, password) => {
+  try {
+      const result = await createUserWithEmailAndPassword(auth, email, password);
+      const user = result.user;
+
+      return user;
+  } catch (error) {
+      logEvent(analytics, 'error on firebaseSignup', error);
+  }
+}  
+
+export const firebaseLogin = async (email, password) => {
+  try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      const user = result.user
+
+      return user;
+  } catch (error) {
+      logEvent(analytics, 'error on firebaseLogin', error);
+  }
+} 
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    // const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
