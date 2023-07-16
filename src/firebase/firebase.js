bc9bd5d28f178b17143dc99d4bd452ad3d323860
Todd Bauermeister <todd@octoco.ltd>
2023-07-16 13:35:26 +0200

    setup firebase
    install firebase package
    setup firebase config file
    initialise firebase app
    initialise firebase analytics
    create firebase db variable
    import firebase into index.js

---
+++ src/firebase/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { firebase } from "firebase";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC8FyOMzpm8gzT8gaZj3lf8pHYxYwnYUVw",
+  authDomain: "react-2023-app-dev.firebaseapp.com",
+  projectId: "react-2023-app-dev",
+  storageBucket: "react-2023-app-dev.appspot.com",
+  messagingSenderId: "266862212297",
+  appId: "1:266862212297:web:e8828d93cbfdf79a720918",
+  measurementId: "G-1XL417H18P"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase Analytics
+const analytics = getAnalytics(app);
+
+// Create a Database Variable ()
+export const database = firebase.firestore();
+export default firebase;
