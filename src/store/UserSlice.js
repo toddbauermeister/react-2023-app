56b730f7f058833f38ef880d20997fc3ca6a0691
Todd Bauermeister <todd@octoco.ltd>
2023-07-23 11:27:32 +0200

    setup base user slice
    add redux store persistence

---
+++ src/store/UserSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = { user: {} }
+
+const UserSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser(state, action) {
+      state.user = action.payload
+    },
+    clearUserAndStore(state) {
+      // NB this also resets the entire redux store for logout purposes
+      state = initialState
+    },
+  },
+})
+
+export const { increment, decrement, incrementByAmount } = UserSlice.actions
+export default UserSlice.reducer
--- src/store/store.js
+++ src/store/store.js
@@ -1,28 +1,42 @@
 import { configureStore, combineReducers, AnyAction } from '@reduxjs/toolkit';
-import counterSlice from '../reducers/counterSlice';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+import UserSlice from './UserSlice';
 
 const combinedReducer = combineReducers({
-  counter: counterSlice,
+  user: UserSlice,
 });
 
 const rootReducer = (state, action) => {
-  // if (action.type === 'user/resetUser') {
-  //   // Reset the Redux store
-  //   state = undefined;
-  // }
+  // Reset the Redux store
+  // For Logout
+  if (action.type === 'user/clearUserAndStore') {
+    state = undefined;
+  }
+
   return combinedReducer(state, action);
 };
 
+// Configure Redux Store Persistence
+const storePersistenceConfig = {
+  key: 'root',
+  storage,
+  whitelist: 'UserSlice'
+}
+
+// Setup Persisted Redux Store
+const persistedReducer = persistReducer(storePersistenceConfig, rootReducer)
+
+// Final Redux Store Setup
 const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(
+    // { serializableCheck: false }
+  ),
   devTools: process.env.REACT_APP_DEPLOYMENT_ENV !== 'production',
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware(
-      // { serializableCheck: false }
-    )
-    // .concat(
-    //   cepApi.middleware,
-    // ),
+  // .concat(
+  //   cepApi.middleware,
+  // ),
 });
 
 export default store;
