3b4b205e5eb096d76b01adcb9a9f250c903db9d5
Todd Bauermeister <todd@octoco.ltd>
2023-07-23 10:43:11 +0200

    add routing and base home page
    install mui/icons-material

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/layout/layout';
+import Home from './components/pages/Home/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './components/pages/SignIn/SignIn';
+import SignUp from './components/pages/Signup/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<SignIn />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Route>
+      </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import firebaseApp from "./firebase/firebase"
 import { Provider } from 'react-redux';
 import store from './store/store'
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
